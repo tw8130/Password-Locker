119a2f7892dde1aeb82bf88b0e3ebfd0081ca10d
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-04-19 19:26:01 +0300

    add test to check if users login  is correct

--- password_test.py
+++ password_test.py
@@ -27,8 +27,33 @@ class TestUser(unittest.TestCase):
         Test to check if the users info is saved in the users_list
         '''
         self.new_user.save_user() #saving the new user
-        self.assertEqual(len(User.users_list),1)
+        self.assertEqual(len(User.users_list),3)
+
+
+class TestCredentials(unittest.TestCase):
+    '''
+    Test class that defines test cases for credentials class behaviours
+    
+    Args:
+    unittest.TestCase:TestCase class to help in creating test cases
+    '''
+    def test_check_user(self):
+        '''
+        Test to check if users login works as it is suppossed to
+        '''
+        self.new_user = User('Tabby','Wanjiku' ,'twpd254')
+        self.new_user.save_user()
+        user2 = User('Nicky' ,'Wanjiku',  'twpd254')
+        user2.save_user()
+
+        for user in User.users_list:
+            if user.first_name == user2.first_name and user.password == user2.password:
+                current_user = user2.first_name
+        return current_user
+
+        self.assertEqual(current_user ,Credential.check_user(user2.password ,user2.first_name))
+
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
         
