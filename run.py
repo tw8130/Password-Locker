f13cf569c07c9767752a6ba68bb508c96383bab7
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-04-20 14:50:14 +0300

    add function to create a new user account

---
+++ run.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3.6
+import pyperclip
+from user_credentials import User , Credential
+
+def create_user(fname,lname,password):
+    '''
+    Function to create a new user account
+    '''
+    new_user = User(fname,lname,password)
+    return new_user
